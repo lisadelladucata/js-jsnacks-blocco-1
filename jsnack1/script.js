589526add927c21940a5b2724f45db4a6b7772c2
lisadelladucata <lisa.delladucata@gmail.com>
2024-12-03 11:36:29 +0100

    feat: add-exercise

---
+++ jsnack1/script.js
@@ -0,0 +1,12 @@
+//  1. L’utente inserisce due numeri in successione, con due prompt.
+// Il software stampa il maggiore
+
+let number1 = prompt ("inserisci un numero")
+let number2 = prompt ("inserisci un numero")
+
+if (number1 > number2){
+    console.log(` Il numero maggiore è: ${number1}` )
+}
+if (number2 > number1){
+    console.log(` Il numero maggiore è: ${number2}` )
+}
